c1f65a542297a2a03918a106850ab3ca33405988
Jotham Yelle <jothamyelle@hotmail.com>
2018-09-14 19:03:53 +0000

    Fixed visitor order functionality

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,9 +4,8 @@ class UserMailer < ApplicationMailer
   def welcome_email(order, line_items)
     @order = order
     @line_items = line_items
-    @user = User.find_by(name: "Jotham")
     @url  = 'http://localhost:3000'
-    mail(to: @user.email, subject: "Order Summary for Order ##{@order.id}")
+    mail(to: @order.email, subject: "Order Summary for Order ##{@order.id}")
   end
 
   def get_product_info product_id
