d72c98b1380145767504e94daeb8f584002a6eec
Jotham Yelle <jothamyelle@hotmail.com>
2018-09-14 19:47:07 +0000

    Displays average ratings on home page and on individual product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,16 @@ class ProductsController < ApplicationController
     @products = Product.all.order(created_at: :desc)
   end
 
+  def get_average(product)
+    @product = product
+    @reviewAvgs = Review.where(product_id: @product.id)
+    @reviewAvgs.average(:rating)
+  end
+  helper_method :get_average
+
   def show
     @reviews = Review.where(product_id: params[:id])
+    @reviewAvg = @reviews.average(:rating)
     @product = Product.find params[:id]
   end
 
